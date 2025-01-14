3ae8561b5b455836e6673a5754d21cf8b32d2add
dps32 <munecodepruebas@gmail.com>
2025-01-14 18:03:29 +0100

    updated website

---
+++ web/assets/js/minigame.js
@@ -0,0 +1,19 @@
+const hitButton = document.querySelector(".hit");
+const standButton = document.querySelector(".stand");
+const counter = document.querySelector(".counter");
+
+const cards = [
+    { url: "assets/images/cards/a.png", value: 1 },
+    { url: "assets/images/cards/2.png", value: 2 },
+    { url: "assets/images/cards/3.png", value: 3 },
+    { url: "assets/images/cards/4.png", value: 4 },
+    { url: "assets/images/cards/5.png", value: 5 },
+    { url: "assets/images/cards/6.png", value: 6 },
+    { url: "assets/images/cards/7.png", value: 7 },
+    { url: "assets/images/cards/8.png", value: 8 },
+    { url: "assets/images/cards/9.png", value: 9 },
+    { url: "assets/images/cards/10.png", value: 10 },
+    { url: "assets/images/cards/j.png", value: 11 },
+    { url: "assets/images/cards/q.png", value: 12 },
+    { url: "assets/images/cards/k.png", value: 13 }
+];
--- web/assets/js/navigation.js
+++ web/assets/js/navigation.js
@@ -1,4 +1,5 @@
-var buttons = document.querySelectorAll(".menu a");
+const buttons = document.querySelectorAll(".menu a");
+const menu = document.querySelector(".menu");
 
 buttons.forEach(function (button) {
     button.addEventListener("click", function (event) {
@@ -6,12 +7,12 @@ buttons.forEach(function (button) {
 
         document.querySelector('.menu a.selected').classList.remove('selected'); // quitamos la clase selected del boton seleccionado
 
-        button.style.pointerEvents = "none"; // deshabilitamos el boton para que la animacion termine
+        menu.style.pointerEvents = "none"; // deshabilitamos el boton para que la animacion termine
         button.style.backgroundColor = "var(--secondary-color)"; // cambiamos el color de fondo del boton
 
         setTimeout(() => {
             var target = event.target.getAttribute("href"); // pillamos el destino del boton
             location.href = target; // redirigimos a la pagina
-            button.style.pointerEvents = "auto"; // habilitamos los eventos del boton por si el usuario vuelve atras
+            menu.style.pointerEvents = "auto"; // habilitamos los eventos del boton por si el usuario vuelve atras
         }, 400);
 })});
