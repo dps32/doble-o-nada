5ac758001d8a9b63f5b57cd995b8e834c0fd5d62
dps32 <munecodepruebas@gmail.com>
2025-01-15 20:00:30 +0100

    arreglada view

--- views/view1 initial_card.sql
+++ views/view1 initial_card.sql
@@ -2,27 +2,29 @@ use sieteymedio;
 
 CREATE VIEW player_initial_card_statistics AS
 SELECT 
-    p.player_id AS player_id,
-    p.name AS player_name,
-    SUBSTRING_INDEX(c.name, ' ', -1) AS suit, -- Palo de la c.inicial más repetida
-    c.name AS most_repeated_card, -- Nombre completo de la c.inicial más repetida
-    COUNT(*) AS times_repeated, -- Número de veces que se ha repetido
-    COUNT(DISTINCT gp.game_id) AS total_games -- Total de partidas jugadas
+    pr.player_id AS player_id,                   -- Identificador del jugador
+    p.name AS player_name,                       -- Nombre del jugador
+    c.priority AS suit,                          -- Palo de la carta
+    c.name AS initial_card_name,                 -- Nombre de la carta inicial
+    COUNT(*) AS times_repeated,                  -- Número de veces que la carta fue la inicial
+    COUNT(DISTINCT r.game_id) AS total_games     -- Total de partidas jugadas por el jugador
 FROM 
-    players p
+    player_rounds pr
 JOIN 
-    player_rounds r ON p.player_id = r.player_id -- Relacionar jugadores con rondas
+    rounds r ON pr.round_id = r.round_id         -- Relación con las rondas
 JOIN 
-    cards c ON c.card_id = r.first_card_in_hand -- Usar columna first_card_in_hand
+    games g ON r.game_id = g.game_id             -- Relación con las partidas
 JOIN 
-    game_players gp ON p.player_id = gp.player_id -- Relacionar jugadores con juegos
+    players p ON pr.player_id = p.player_id      -- Relación con los jugadores
+JOIN 
+    cards c ON pr.first_card_in_hand = c.card_id -- Relación con las cartas
 WHERE 
-    r.round_number = 1 -- Considerar solo la primera carta de cada ronda
+    r.round_number = 1                           -- Solo considerar la primera ronda
 GROUP BY 
-    p.player_id, c.card_id -- Agrupar por jugador y carta inicial
-HAVING 
-    total_games >= 3 -- Solo players en 3 o más partidas
+    pr.player_id, c.card_id                      -- Agrupar por jugador y carta inicial
+HAVING
+	total_games > 3                              -- Colegon cuenta el total de juegos
 ORDER BY 
-    player_id, times_repeated DESC; -- ordenado descendente.
+    pr.player_id, times_repeated DESC;           -- Ordenar por jugador y veces repetidas
 
 SELECT * FROM player_initial_card_statistics;
