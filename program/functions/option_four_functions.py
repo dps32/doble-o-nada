f2b2e779a606dd406bec336aeb01125b4fbede15
monica438 <monica14dominguez@gmail.com>
2025-01-20 00:32:37 +0100

    update ranking (tambien acepta si es solo 1 fila)

--- program/functions/option_four_functions.py
+++ program/functions/option_four_functions.py
@@ -8,16 +8,17 @@ def rankingMoreEarnings():
     print("*"*136 + titles.title_more_earnings_centred + "*"*136)
     ranking_earnings = list(ranking.keys())
 
-    for sweep in range(len(ranking_earnings) - 1):  # Ordenamos la lista por los earnings
-        cambio = False
-        for i in range(len(ranking_earnings) - (sweep + 1)):
-            if ranking[ranking_earnings[i+1]]["total_gains"] > ranking[ranking_earnings[i]]["total_gains"]:
-                cambio = True
-                aux = ranking_earnings[i+1]
-                ranking_earnings[i+1] = ranking_earnings[i]
-                ranking_earnings[i] = aux
-        if not cambio:
-            break
+    if len(ranking_earnings) > 1:
+        for sweep in range(len(ranking_earnings) - 1):  # Ordenamos la lista por los earnings
+            cambio = False
+            for i in range(len(ranking_earnings) - (sweep + 1)):
+                if ranking[ranking_earnings[i+1]]["total_gains"] > ranking[ranking_earnings[i]]["total_gains"]:
+                    cambio = True
+                    aux = ranking_earnings[i+1]
+                    ranking_earnings[i+1] = ranking_earnings[i]
+                    ranking_earnings[i] = aux
+            if not cambio:
+                break
 
     data = "*"*90 + "\n" + "DNI".ljust(15) + "Name".ljust(25) + "Earnings".rjust(15) + "Games Played".rjust(15) + "Minutes Played".rjust(20) + "\n" + "*"*90 + "\n"
     for dni in ranking_earnings:
@@ -32,16 +33,17 @@ def rankingMoreGames():
 
     ranking_games = list(ranking.keys())
 
-    for sweep in range(len(ranking_games) - 1):  # Ordenamos la lista por los earnings
-        cambio = False
-        for i in range(len(ranking_games) - (sweep + 1)):
-            if ranking[ranking_games[i + 1]]["total_games"] > ranking[ranking_games[i]]["total_games"]:
-                cambio = True
-                aux = ranking_games[i + 1]
-                ranking_games[i + 1] = ranking_games[i]
-                ranking_games[i] = aux
-        if not cambio:
-            break
+    if len(ranking_games) > 1:
+        for sweep in range(len(ranking_games) - 1):  # Ordenamos la lista por los earnings
+            cambio = False
+            for i in range(len(ranking_games) - (sweep + 1)):
+                if ranking[ranking_games[i + 1]]["total_games"] > ranking[ranking_games[i]]["total_games"]:
+                    cambio = True
+                    aux = ranking_games[i + 1]
+                    ranking_games[i + 1] = ranking_games[i]
+                    ranking_games[i] = aux
+            if not cambio:
+                break
 
     data = "*" * 90 + "\n" + "DNI".ljust(15) + "Name".ljust(25) + "Earnings".rjust(15) + "Games Played".rjust(
         15) + "Minutes Played".rjust(20) + "\n" + "*" * 90 + "\n"
@@ -58,16 +60,17 @@ def rankingMoreMinutes():
 
     ranking_minutes = list(ranking.keys())
 
-    for sweep in range(len(ranking_minutes) - 1):  # Ordenamos la lista por los earnings
-        cambio = False
-        for i in range(len(ranking_minutes) - (sweep + 1)):
-            if ranking[ranking_minutes[i + 1]]["total_minutes"] > ranking[ranking_minutes[i]]["total_minutes"]:
-                cambio = True
-                aux = ranking_minutes[i + 1]
-                ranking_minutes[i + 1] = ranking_minutes[i]
-                ranking_minutes[i] = aux
-        if not cambio:
-            break
+    if len(ranking_minutes) > 1:
+        for sweep in range(len(ranking_minutes) - 1):  # Ordenamos la lista por los earnings
+            cambio = False
+            for i in range(len(ranking_minutes) - (sweep + 1)):
+                if ranking[ranking_minutes[i + 1]]["total_minutes"] > ranking[ranking_minutes[i]]["total_minutes"]:
+                    cambio = True
+                    aux = ranking_minutes[i + 1]
+                    ranking_minutes[i + 1] = ranking_minutes[i]
+                    ranking_minutes[i] = aux
+            if not cambio:
+                break
 
     data = "*" * 90 + "\n" + "DNI".ljust(15) + "Name".ljust(25) + "Earnings".rjust(15) + "Games Played".rjust(
         15) + "Minutes Played".rjust(20) + "\n" + "*" * 90 + "\n"
