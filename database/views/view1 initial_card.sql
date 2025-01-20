1206b135f75cab2ffb3f3584ef8df284b911373c
ElTitoKino97 <cris97720@gmail.com>
2025-01-20 16:13:24 +0100

    update initial card

--- database/views/view1 initial_card.sql
+++ database/views/view1 initial_card.sql
@@ -1,28 +1,27 @@
 use sieteymedio;
-
-CREATE OR REPLACE VIEW player_initial_card_statistic AS
+CREATE VIEW player_initial_card_statistics AS
 SELECT 
-    p.player_id AS player_id, -- Identificador del jugador
-    p.name AS player_name, -- Nombre del jugador
-    SUBSTRING_INDEX(c.name, ' ', -1) AS suit, -- Palo de la carta inicial más repetida
-    c.name AS most_repeated_card, -- Nombre completo de la carta inicial más repetida
-    COUNT(*) AS times_repeated, -- Número de veces que se ha repetido
-    COUNT(DISTINCT r.game_id) AS total_games -- Total de partidas jugadas
+    p.player_id AS player_id, -- ID del player
+    p.name AS player_name, -- name del player
+    SUBSTRING_INDEX(c.name, ' ', -1) AS suit, -- Palo de la carta 
+    c.name AS most_repeated_card, -- Nombre completo de la carta 
+    COUNT(*) AS times_repeated, -- Número de veces repetida
+    COUNT(DISTINCT r.game_id) AS total_games -- Total games
 FROM 
     players p
 JOIN 
-    player_rounds pr ON p.player_id = pr.player_id -- Relación jugadores y rondas
+    player_rounds pr ON p.player_id = pr.player_id -- Relación jugadores-rondas
 JOIN 
-    rounds r ON pr.round_id = r.round_id -- Relación rondas y partidas
+    rounds r ON pr.round_id = r.round_id -- Relación rondas-partidas
 JOIN 
     games g ON r.game_id = g.game_id -- Relación partidas
 JOIN 
     cards c ON pr.first_card_in_hand = c.card_id -- Relación cartas iniciales
 WHERE 
-    r.round_number = 0 -- Considerar solo la ronda inicial (ronda 0)
+    r.round_number = 0 -- Considerar primera ronda
 GROUP BY 
-    p.player_id, c.card_id -- Agrupar por jugador y carta inicial
+    p.player_id, c.card_id, suit -- Agrupar jugador y carta inicial
 HAVING 
-    total_games >= 3 -- Solo jugadores con al menos 3 partidas
+    COUNT(DISTINCT r.game_id) >= 3 -- Solo players que hayan jugado al menos 3 partidas
 ORDER BY 
-    player_id, times_repeated DESC; -- Ordenar por jugador y frecuencia
+    p.player_id, times_repeated DESC; -- Ordenar por jugador y frecuencia
