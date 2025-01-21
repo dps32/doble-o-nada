01f66d24292ba504eb65abe7d7c79c71e303c2b7
ElTitoKino97 <cris97720@gmail.com>
2025-01-21 18:31:43 +0100

    Update view2 high_bet.sql

--- database/views/view2 high_bet.sql
+++ database/views/view2 high_bet.sql
@@ -2,24 +2,23 @@ use sieteymedio;
 
 CREATE VIEW highest_bet_per_game AS
 SELECT
-    r.game_id AS game_id, -- Identificador de la partida
-    pr.player_id AS player_id, -- Identificador del jugador
-    MAX(pr.player_bet) AS highest_bet -- Apuesta más alta en la partida
-FROM 
-    player_rounds pr -- Tabla de rondas por jugador
-JOIN 
-    rounds r ON pr.round_id = r.round_id -- Relacionar las rondas con las partidas
-GROUP BY 
-    r.game_id, pr.player_id -- Agrupar por partida y jugador
-HAVING 
-    highest_bet = (
-        SELECT MAX(pr2.player_bet)
-        FROM player_rounds pr2
+    r.game_id,
+    pr.player_id,
+    pr.player_bet
+FROM
+    player_rounds pr
+JOIN rounds r ON pr.round_id = r.round_id
+WHERE
+    (r.game_id, pr.player_bet, pr.player_id) IN (
+        SELECT
+            r2.game_id,
+            MAX(pr2.player_bet) AS max_bet,
+            MIN(pr2.player_id) AS min_player_id
+        FROM
+            player_rounds pr2
         JOIN rounds r2 ON pr2.round_id = r2.round_id
-        WHERE r2.game_id = r.game_id
+        GROUP BY
+            r2.game_id
     )
-ORDER BY 
-    highest_bet DESC, -- Ordenar por apuesta más alta (descendente)
-    pr.player_id ASC; -- En caso de empate, mostrar el jugador con menor ID
-    
-SELECT * FROM highest_bet_per_game;
+ORDER BY
+    pr.player_bet DESC;
